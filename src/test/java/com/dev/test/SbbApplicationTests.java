8944d298c49703859797d9a22624845b6e304d4b
chung hwan lim <dlacndghks12@gmail.com>
2025-03-24 00:30:57 +0900

    first commit

---
+++ src/test/java/com/dev/test/SbbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
